5d2573a3ae81e6a7055718c6c7ace1e9b17a7b6e
cgillila <cgillila@nd.edu>
2021-11-29 11:45:45 -0500

    edits

--- analysis.R
+++ analysis.R
@@ -1,3 +1,29 @@
+# initialize
 rm(list=ls())
-
 source("supportingFunctions.R")
+library(ggplot2)
+
+# takes in a directory and string, the string NA_rows must be
+# "remove", "warn", or "include"
+compile_data<-function(directory, NA_rows){
+  # get all the files names
+  file_names <- list.files(path=directory, pattern=".csv", full.names=T)
+  
+  # deal with the NAs accordingly
+  rmNA <- F
+  warn <- F
+  if (NA_rows == "remove") {
+    rmNA <- T
+  }
+  if (NA_rows == "warn") {
+    warn <- T
+  }
+  
+  for (file in file_names) {
+    temp_data = read.csv(file, header=T)
+  }
+  return (file_names)
+}
+
+result <- compile_data(directory = "countryX", NA_rows = "remove")
+print(result)
